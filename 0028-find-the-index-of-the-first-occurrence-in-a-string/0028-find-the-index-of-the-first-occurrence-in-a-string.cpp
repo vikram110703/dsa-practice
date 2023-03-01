aaff386f2b252e4c35d9328344fd8f33643d7c30
vikram singh <99256244+vikram110703@users.noreply.github.com>
2023-03-02 02:25:24 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (94.99%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     int strStr(string s, string ss) {
-        int n=s.size(),m=ss.size();
         int ans=-1;
         
-        for(int i=0;i<=n-m;i++)
-        {
-                if(s.substr(i,m)==ss)
-                {
-                    //store answer
-                    ans=i;
-                    break;
-                }
-        }
+        auto it=s.find(ss);
+        if(it!=s.size())ans=it;
+     
      return ans;   
     }
 };
