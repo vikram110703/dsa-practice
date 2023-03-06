3bc38d65de96dae2a640c6eb4d1264b520ddc148
vikram singh <99256244+vikram110703@users.noreply.github.com>
2023-03-06 10:27:34 +0530

    Time: 3 ms (88.86%), Space: 9.5 MB (73.33%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -4,46 +4,41 @@ class Solution
         int findKthPositive(vector<int> &arr, int k)
         {
             int n = arr.size();
+           	//  checking missing elemetns before arr[0]
             int strt = arr[0] - 1;
+           	//kth missing element is > arr[0]
             if (k >= strt)
             {
                 k -= strt;
             }
+           	// kth missing element is < arr[0]
             else
             {
                 return k;
             }
             if (k == 0) return strt;
-            int missing;
+
+           	//  checking Kth missing element in the range[arr[0],arr[n-1]]
             for (int i = 0; i < n - 1; i++)
             {
-                missing = arr[i];
-                if (arr[i + 1] - missing > 1)
-                {
-                    missing+=1;
-                    while(missing<arr[i+1])
-                    {
-                        k-=1;
-                        // cout << missing << " ";
-                        if (k == 0)
-                        {
-                            return missing;
-                        }
-                        missing += 1;
-                    }
 
-                    // while (arr[i + 1] - missing >= 1)
-                    // {
-                    //     cout << arr[i] << " ";
-                    //     if (k == 0)
-                    //     {
-                    //         return missing;
-                    //     }
-                    //     missing += 1;
-                    //     k -= 1;
-                    // }
+                if (arr[i + 1] - arr[i] > 1)
+                {
+                   	//total missing elements between arr[i]--arr[i+1]
+                    int missing = arr[i + 1] - arr[i]-1;
+                   	//missing is >=k
+                    if (missing >= k)
+                    {
+                        return arr[i] + k;
+                    }
+                    else
+                    {
+                        k -= missing;
+                    }
                 }
             }
+
+           	// if missing element is >arr[n-1]
             int last = arr[n - 1] + (k);
             return last;
         }
