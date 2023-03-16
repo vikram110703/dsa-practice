f7577a81fb467501a6fc5aa147bac2f8254a1feb
vikram singh <99256244+vikram110703@users.noreply.github.com>
2023-03-16 11:53:02 +0530

    Time: 18 ms (71.95%), Space: 26.5 MB (48.48%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,49 +1,47 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
 
-       TreeNode* construct(vector<int> &inorder, vector<int> &postorder, int &rootIndx, int strt, int end)
-{
-    if (rootIndx < 0 || strt < 0 || end >= inorder.size() || strt > end) return NULL;
-    TreeNode * tmp = new TreeNode(postorder[rootIndx]);
-
-    // find index of root element in inorder 
-    int indx;
-    for (int i = 0; i < inorder.size(); i++)
-    {
-        if (inorder[i] == postorder[rootIndx])
+        TreeNode* construct(vector<int> &inorder, vector<int> &postorder, int &rootIndx, int strt, int end,map<int,int>&mpp)
         {
-            indx = i;
-            break;
+            if (rootIndx < 0 || strt < 0 || end >= inorder.size() || strt > end) return NULL;
+            TreeNode *tmp = new TreeNode(postorder[rootIndx]);
+
+           	// find index of root element in inorder 
+            int indx=mpp[postorder[rootIndx]];
+      
+            
+            rootIndx -= 1;
+
+           	// construct right subtree
+            tmp->right = construct(inorder, postorder, rootIndx, indx + 1, end,mpp);
+           	// construct left subtree
+            tmp->left = construct(inorder, postorder, rootIndx, strt, indx - 1,mpp);
+
+            return tmp;
         }
-    }
-    rootIndx -= 1;
-           
-           // construct right subtree
-    tmp->right = construct(inorder, postorder, rootIndx, indx + 1, end);
-    // construct left subtree
-    tmp->left = construct(inorder, postorder, rootIndx, strt, indx - 1);
-    
-    return tmp;
-}
 
     TreeNode* buildTree(vector<int> &inorder, vector<int> &postorder)
     {
         int n = inorder.size();
-       
-        int rootIndx=n-1;
-        return construct(inorder, postorder, rootIndx, 0, n - 1);
-        
+        map<int,int>mpp;
+        for(int i=0;i<n;i++)
+        {
+            mpp[inorder[i]]=i;
+        }
+
+        int rootIndx = n - 1;
+        return construct(inorder, postorder, rootIndx, 0, n - 1,mpp);
     }
 };
