a35fe47877bae718a1bc95f14810a3c92c0aa404
vikram singh <99256244+vikram110703@users.noreply.github.com>
2023-03-22 13:09:53 +0530

    Time: 842 ms (10.47%), Space: 153.1 MB (14.16%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,46 @@
+class Solution
+{
+    public:
+
+        void bfs(int node, map<int, vector<pair<int, int>>> &adj, map< int, bool> &viss, int &mini)
+        {
+            queue<int> q;
+            q.push(node);
+           	// viss[node]=true;
+            while (!q.empty())
+            {
+                int fnode = q.front();
+                q.pop();
+                if (viss[fnode] == false)
+                {
+                    viss[fnode] = true;
+
+                    for (auto it: adj[fnode])
+                    {
+                        mini = min(mini, it.second);
+                        if (viss[it.first] == false) q.push(it.first);
+                    }
+                }
+            }
+        }
+
+    int minScore(int n, vector<vector < int>> &v)
+    {
+        map<int, vector<pair<int, int>>> adj;
+        int nn = v.size();	// size of v
+        for (int i = 0; i < nn; i++)
+        {
+            int a = v[i][0], b = v[i][1], c = v[i][2];
+            adj[a].push_back({ b,
+                c });
+            adj[b].push_back({ a,
+                c });
+        }
+        map<int, bool> viss;
+        for (int i = 0; i < n; i++) viss[i + 1] = false;
+        int mini = INT_MAX;
+
+        bfs(1, adj, viss, mini);
+        return mini;
+    }
+};
