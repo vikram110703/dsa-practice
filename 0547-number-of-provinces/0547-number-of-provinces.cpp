f6f291d03667b4963e1ea5dc8136d38c1800c1ac
vikram singh <99256244+vikram110703@users.noreply.github.com>
2023-05-17 16:26:49 +0530

    Time: 32 ms (23.23%), Space: 15.1 MB (20.51%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,58 +1,48 @@
 class Solution
 {
     public:
-
-        void bfs(vector<vector < int>> &adj, int ind, map< int, bool> &viss)
+        void dfs(int n, vector<int> adj[], vector<int> &viss)
         {
-            queue<int> q;
-            q.push(ind);
-            viss[ind] = true;
-
-            while (!q.empty())
+            viss[n]=true;
+            for (auto child: adj[n])
             {
-                int fnode = q.front();
-                q.pop();
-
-                for (auto &child: adj[fnode])
+                if (viss[child] == 0)
                 {
-                    if (viss[child] == false)
-                    {
-                       q.push(child);
-                         viss[child] = true;
-                    }
+                    viss[child] = 1;
+                    dfs(child, adj, viss);
                 }
             }
         }
-
 
     int findCircleNum(vector<vector < int>> &isConnected)
     {
         int n = isConnected.size();
-
-        vector<vector < int>> adj(n + 1);
+        vector<int> adj[n + 1];
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                if (i!=j&&isConnected[i][j] == 1)
+                if (i != j)
+
                 {
-                    adj[i + 1].push_back(j + 1);
-                    adj[j + 1].push_back(i + 1);
+                    if (i!=j&&isConnected[i][j] == 1)
+                    {
+                        adj[i + 1].push_back(j + 1);
+                        adj[j + 1].push_back(i + 1);
+                    }
                 }
             }
         }
-
-        map<int, bool> viss;
-        int disConnectedComp = 0;
+        vector<int> viss(n + 1, 0);
+        int cnt = 0;
         for (int i = 1; i <= n; i++)
         {
-            if (viss[i] == false)
+            if (viss[i] == 0)
             {
-                bfs(adj, i, viss);
-                disConnectedComp += 1;
+                dfs(i, adj, viss);
+                cnt += 1;
             }
         }
-
-        return disConnectedComp;
+        return cnt;
     }
 };
