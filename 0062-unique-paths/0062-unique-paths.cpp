caad2fde90bed514533beb9a567996796b4e1757
vikram singh <99256244+vikram110703@users.noreply.github.com>
2023-05-17 10:04:12 +0530

    Time: 5 ms (17.02%), Space: 6.4 MB (63.56%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
-    
     int uniquePaths(int m, int n) {
-        int dp[m][n];
-        for(int i=0;i<m;i++)
+        
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
-                dp[i][j]=0;
+                if(i==0&&j==0)
+                {
+                    dp[i][j]=1;continue;
+                }
+                
+                int upper=0,left=0;
+                if(i-1>=0)
+                {
+                    upper=dp[i-1][j];
+                }
+                if(j-1>=0)
+                {
+                    left=dp[i][j-1];
+                }
+                dp[i][j]=upper+left;
             }
         }
-        dp[0][0]=1;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(i==0&&j==0)continue;
-                if(i-1>=0)dp[i][j]+=dp[i-1][j];
-                if(j-1>=0)dp[i][j]+=dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
+        int ans=0;
+        if(n-2>=0)ans+=dp[n-2][m-1];
+        if(m-2>=0)ans+=dp[n-1][m-2];
+        return max(ans,1);
+        
     }
 };
