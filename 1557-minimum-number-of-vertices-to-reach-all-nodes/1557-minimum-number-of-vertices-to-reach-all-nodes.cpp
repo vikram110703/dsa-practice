0e9888ed418d302f6e945e25a5b57db073039942
vikram singh <99256244+vikram110703@users.noreply.github.com>
2023-05-18 15:39:41 +0530

    Time: 431 ms (13.44%), Space: 106.8 MB (28.35%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,44 +1,18 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>&adj,int node,map<int,bool>&viss,map<int,bool>&inDeg)
-{
-    viss[node]=true;
-        for(auto &child:adj[node])
-        {
-            
-                inDeg[child]=true;
-            if(viss[child]==false)
-                dfs(adj,child,viss,inDeg);
-            
-        }
-}    
-    
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        vector<vector<int>>adj(n);
+        map<int,bool>inDeg;
         for(int i=0;i<edges.size();i++)
         {
             int a=edges[i][0];
             int b=edges[i][1];
-            adj[a].push_back(b);
+            inDeg[b]=true;
         }
-        map<int,bool>viss,inDeg;
         vector<int>ans;
         for(int i=0;i<n;i++)
         {
-            if(viss[i]==false)
-            {
-                dfs(adj,i,viss,inDeg);
-            }
+            if(inDeg[i]==false)ans.push_back(i);
         }
-        for(int i=0;i<n;i++)
-        {
-            if(inDeg[i]==false)
-            {
-                ans.push_back(i);
-            }
-        }
-        
         return ans;
-        
     }
 };
