d6c63920f21924b5b6a1772db3c44afba7c39081
vikram singh <99256244+vikram110703@users.noreply.github.com>
2023-06-22 08:37:48 +0530

    Time: 71 ms (76.26%), Space: 19.7 MB (66.32%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,131 +1,34 @@
 class Solution {
 public:
-    //shortest path
-    void bfs(int indx,vector<vector<int>>&adj,vector<int>&parent,unordered_map<int,bool>viss)
-    {
-         queue<int>q;
-            q.push(indx);
-            viss[indx]=true;
-            while(!q.empty())
-            {
-               int fnode=q.front();
-                q.pop();
-                for(auto &child:adj[fnode])
-                {
-                    if(viss[child]==false)
-                    {
-                        parent[child]=fnode;
-                        viss[child]=true;
-                        q.push(child);
-                    }
-                }
-            }
-    }
-    
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        
         int n=grid.size();
-        if(grid[0][0]==1||grid[n-1][n-1]==1)
-        {
-            return -1;
-        }
-        vector<vector<int>>adj((n*n)+1);
-        map<pair<int,int>,int>ind;
-        unordered_map<int,bool>viss;
-        vector<int>parent((n*n)+1,-1);
+        if(grid[0][0]==1||grid[n-1][n-1]==1)return -1;
         
-        int indx=1;
-        for(int i=0;i<n;i++)
+        pair<int,int>dirs[8]={{1,0},{0,1},{0,-1},{-1,0},{1,-1},{-1,1},{-1,-1},{1,1}};
+        queue<pair<int,int>>q;
+        //{dx,dy}
+        q.push({0,0});
+        grid[0][0]=1;
+        while(!q.empty())
         {
-            for(int j=0;j<n;j++)
+            pair<int,int>curr;
+             curr.first=q.front().first;
+             curr.second=q.front().second;
+            if(curr.first==n-1&&curr.second==n-1)return grid[n-1][n-1];
+            q.pop();
+            for(auto it:dirs)
             {
-                ind[{i,j}]=indx++;
+                int dx=curr.first+it.first;
+                int dy=curr.second+it.second;
+                // for viss cond. wr r checking grid[dx][dy]==0
+                if(dx>=0&&dy>=0&&dx<n&&dy<n&&grid[dx][dy]==0)
+                {
+                    q.push({dx,dy});
+                    grid[dx][dy]=grid[curr.first][curr.second]+1;
+                }
             }
         }
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-               if(i-1>=0&&grid[i-1][j]==0&&grid[i][j]==0)
-               {
-                    int indx1=ind[{i,j}];
-                   int  indx2=ind[{i-1,j}];
-                   adj[indx1].push_back(indx2);
-                   adj[indx2].push_back(indx1);
-               }
-                 if(i-1>=0&&j-1>=0&&grid[i-1][j-1]==0&&grid[i][j]==0)
-               {
-                    int indx1=ind[{i,j}];
-                   int  indx2=ind[{i-1,j-1}];
-                   adj[indx1].push_back(indx2);
-                   adj[indx2].push_back(indx1);
-               }
-                
-                 if(i-1>=0&&j+1<n&&grid[i-1][j+1]==0&&grid[i][j]==0)
-               {
-                    int indx1=ind[{i,j}];
-                   int  indx2=ind[{i-1,j+1}];
-                   adj[indx1].push_back(indx2);
-                   adj[indx2].push_back(indx1);
-               }
-                
-                  if(i+1<n&&j+1<n&&grid[i+1][j+1]==0&&grid[i][j]==0)
-               {
-                    int indx1=ind[{i,j}];
-                   int  indx2=ind[{i+1,j+1}];
-                   adj[indx1].push_back(indx2);
-                   adj[indx2].push_back(indx1);
-               }
-                 if(i+1<n&&grid[i+1][j]==0&&grid[i][j]==0)
-               {
-                    int indx1=ind[{i,j}];
-                   int  indx2=ind[{i+1,j}];
-                   adj[indx1].push_back(indx2);
-                   adj[indx2].push_back(indx1);
-               }
-                  if(i+1<n&&j-1>=0&&grid[i+1][j-1]==0&&grid[i][j]==0)
-               {
-                    int indx1=ind[{i,j}];
-                   int  indx2=ind[{i+1,j-1}];
-                   adj[indx1].push_back(indx2);
-                   adj[indx2].push_back(indx1);
-               }
-                
-                if(j-1>=0&&grid[i][j-1]==0&&grid[i][j]==0)
-                {
-                    int indx1=ind[{i,j}];
-                   int  indx2=ind[{i,j-1}];
-                   adj[indx1].push_back(indx2);
-                   adj[indx2].push_back(indx1);
-                }
-                if(j+1<n&&grid[i][j+1]==0&&grid[i][j]==0)
-                {
-                    int indx1=ind[{i,j}];
-                   int  indx2=ind[{i,j+1}];
-                   adj[indx1].push_back(indx2);
-                   adj[indx2].push_back(indx1);
-                }
-                
-                
-            }
-        }
-        
-        //shortest path frm 1 to n*n;
-        int indxx=1;
-        bfs(indxx,adj,parent,viss);
-        int cnt=1;
-        int lastInd=n*n;
-        while(parent[lastInd]!=-1)
-        {
-            cnt+=1;
-            lastInd=parent[lastInd];
-        }
-        if(lastInd!=1)return -1;
-        else
-        return cnt;
-      
-        
+        return -1;
         
     }
 };
