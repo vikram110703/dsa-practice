ba58016216cd69887aa79f052aa77ec180e7da36
vikram singh <99256244+vikram110703@users.noreply.github.com>
2023-07-10 17:32:20 +0530

    Time: 271 ms (91.50%), Space: 144.5 MB (85.25%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -1,40 +1,88 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    int minD(TreeNode* tmp,int mini,int cnt)
+class Solution
+{
+    public:
+        int minD(TreeNode *tmp, int mini, int cnt)
+        {
+            if (tmp == NULL)
+            {
+                return 0;
+            }
+            if (tmp->left == NULL && tmp->right == NULL)
+            {
+                return cnt;
+            }
+            int left = INT_MAX, right = INT_MAX;
+            if (tmp->left)
+            {
+                left = minD(tmp->left, mini, cnt + 1);
+            }
+            else if (tmp->right)
+            {
+                right = minD(tmp->right, mini, cnt + 1);
+            }
+            mini = min({ mini,
+                left,
+                right });
+            return mini;
+        }
+
+    int levelOrder(TreeNode *root)
     {
-        if(tmp==NULL)
+        if (root == NULL)
         {
             return 0;
         }
-        if(tmp->left==NULL&&tmp->right==NULL)
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        int level = 1;
+        while (!q.empty())
         {
-            return cnt;
+            TreeNode *tmp = q.front();
+            q.pop();
+            if (tmp == NULL)
+            {
+                if (q.size() == 0)
+                {
+                    return level;
+                }
+                else
+                {
+                    q.push(NULL);
+                    level += 1;
+                }
+            }
+            else
+            {
+                 if (tmp->left == NULL && tmp->right == NULL)
+                {
+                    return level;
+                }
+                if (tmp->left) q.push(tmp->left);
+                if (tmp->right) q.push(tmp->right);
+            }
         }
-        int left=INT_MAX,right=INT_MAX;
-        if(tmp->left){
-            left=minD(tmp->left,mini,cnt+1);
-        }
-        if(tmp->right)
-        {
-            right=minD(tmp->right,mini,cnt+1);
-        }
-        mini=min({mini,left,right});
-        return mini;
+        return level;
     }
-    
-    int minDepth(TreeNode* root) {
-        
-        return minD(root,INT_MAX,1);
+
+    int minDepth(TreeNode *root)
+    {
+
+       	// return minD(root, INT_MAX, 1);
+
+       	// sol-2 : levelOrderTraversal
+
+        return levelOrder(root);
     }
 };
