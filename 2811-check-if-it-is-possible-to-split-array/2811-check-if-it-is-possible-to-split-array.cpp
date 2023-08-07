29ff9327bc07da778c055916cde4f10ce12160b6
vikram singh <99256244+vikram110703@users.noreply.github.com>
2023-08-07 10:18:51 +0530

    Time: 6 ms (83.33%), Space: 17.7 MB (66.67%) - LeetHub

---
+++ 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canSplitArray(vector<int>& nums, int m) {
+        int n=nums.size();
+        if(n==1||n==2)return true;
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i]+nums[i+1]>=m)
+            {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
