4608c65ade7cacbd4a3680f7f344b87b8228831a
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-08-16 22:32:23 +0530

    Time: 345 ms (28.77%), Space: 150.3 MB (20.29%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n=nums.size();
-        multiset<int>st;
-        // priority_queue<int>pq;
-        int maxi=INT_MIN;
-        if(n==1)
-        {
-            return {nums[0]};
-        }
-        if(k==1)
-        {
-            return nums;
-        }
-        for(int i=0;i<k;i++)
-        {
-           st.insert(nums[i]);
-            maxi=max(maxi,nums[i]);
-        }
-        vector<int>ans;
-        ans.push_back(maxi);
+    
+    
+void build(int ind, int low, int high, vector<int> &a, vector<int> &seg)
+{
+    if (low == high)
+    {
+        seg[ind] = a[low];
+        return;
+    }
+    int mid = low + (high - low) / 2;
+    build(2 * ind + 1, low, mid, a, seg);
+    build(2 * ind + 2, mid + 1, high, a, seg);
+    seg[ind] = max(seg[(2 * ind) + 1],seg[(2 * ind) + 2]);
+}
+
+int query(int ind, int low, int high, int l, int r, vector<int> &seg)
+{
+    if (high < l || low > r)
+        return INT_MIN;
+    if (low >= l && high <= r)
+        return seg[ind];
+    int mid = low + (high - low) / 2;
+    int left = query((2 * ind) + 1, low, mid, l, r, seg);
+    int right = query((2 * ind) + 2, mid + 1, high, l, r, seg);
+    return max(left,right);
+}
+    
+    vector<int> maxSlidingWindow(vector<int>& a, int k) {
+        int n=a.size();
+        vector<int>seg(4*n,0),ans;
+        build(0,0,n-1,a,seg);
         int strt=0;
-        for(int i=k;i<n;i++)
+        for(int i=k-1;i<n;i++)
         {
-            int old=nums[strt];
-            int neww=nums[i];
-            
-            auto it=st.lower_bound(old);
-            st.erase(it);
-            auto itt=st.end();
-            itt--;
-                int curMax=*itt;
-            if(neww>=curMax){
-                maxi=neww;
-            }
-            else{
-                maxi=curMax;
-            }
-            st.insert(neww);
+            int maxi=query(0,0,n-1,strt,i,seg);
+            // cout<<strt<<" "<<k<<endl;
             ans.push_back(maxi);
-            strt+=1;
+            strt++;
         }
         return ans;
+       
     }
 };
