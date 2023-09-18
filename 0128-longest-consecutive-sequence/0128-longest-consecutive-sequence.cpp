e92898a5d612e42d024a806c02c8c1c9d2ded855
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-09-18 16:01:42 +0530

    Time: 143 ms (52.38%), Space: 67.3 MB (28.19%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-    
-    int longestConsecutive(vector<int>& nums) {
-        int n=nums.size();
-        if(n==0)return 0;
-        set<int>st;
-        for(int i=0;i<n;i++)
-        {
-            st.insert(nums[i]);
-        }
-        vector<int>vv;
-        for(auto it:st)vv.push_back(it);
-        // sort(nums.begin(),nums.end());
-        n=vv.size();
-        int ans=0;
-        int curr=0;
+    int longestConsecutive(std::vector<int>& nums) {
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int best = 0;
         
-   // [1,2,0,1]-> 0 1 1 2 
-        
-        for(int i=1;i<n;i++)
-        {
-            curr=0;
-            while(i<n&&vv[i]-vv[i-1]==1)
-            {
-                i+=1;curr+=1;
+        for (int x : numSet) {
+            if (numSet.find(x - 1) == numSet.end()) {
+                int y = x + 1;
+                while (numSet.find(y) != numSet.end()) {
+                    y++;
+                }
+                best = std::max(best, y - x);
             }
-            //
-            if(curr>0)curr+=1;
-           ans=max({ans,curr,1});
-            
-            
         }
-        return max(ans,1);
         
+        return best;
     }
 };
+
+// st.find(x)-> ~~O(1)
