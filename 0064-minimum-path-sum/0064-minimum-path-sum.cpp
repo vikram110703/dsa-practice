0ae303d0d84c4157d9e4fa2c065c52bc1f999d09
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-10-09 22:55:29 +0530

    Time: 8 ms (44.58%), Space: 10.3 MB (20.91%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
+    
+    int cal(int row,int col,vector<vector<int>>&grid,vector<vector<int>>&dp){
+        if(row==0&&row==col)return dp[row][col]=grid[row][col];
+        if(row<0||col<0)return INT_MAX;
+        if(dp[row][col]!=-1)return dp[row][col];
+        
+        return dp[row][col]= grid[row][col]+ min(cal(row-1,col,grid,dp),cal(row,col-1,grid,dp));
+        
+    }
+    
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size(),m=grid[0].size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(i==0&&j==0)
-                {
-                    dp[i][j]=grid[i][j];
-                }
-                else
-                {
-                    int upper=INT_MAX,left=INT_MAX;
-                    if(i-1>=0)upper=dp[i-1][j];
-                    if(j-1>=0)left=dp[i][j-1];
-                    dp[i][j]=(min(left,upper))+grid[i][j];
-                }
-            }
-        }
         
-        return dp[n-1][m-1];
+        return cal(n-1,m-1,grid,dp);
+        
     }
 };
