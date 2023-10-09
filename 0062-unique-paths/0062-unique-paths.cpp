870b2a9945f3a2de9c89d3fb15e8ee6b8cb4c4a2
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-10-09 22:28:23 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (10.05%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,33 +1,30 @@
-class Solution {
-public:
-    int uniquePaths(int m, int n) {
-        
-        vector<vector<int>>dp(n,vector<int>(m,0));
-        for(int i=0;i<n;i++)
+class Solution
+{
+    public:
+
+        int cal(int row, int col, vector<vector < int>> &dp)
         {
-            for(int j=0;j<m;j++)
+            if (row == 0 && row == col)
             {
-                if(i==0&&j==0)
-                {
-                    dp[i][j]=1;continue;
-                }
-                
-                int upper=0,left=0;
-                if(i-1>=0)
-                {
-                    upper=dp[i-1][j];
-                }
-                if(j-1>=0)
-                {
-                    left=dp[i][j-1];
-                }
-                dp[i][j]=upper+left;
+                return dp[row][col] = 1;
             }
+            if (dp[row][col] != -1) return dp[row][col];
+
+            if (row == 0)
+            {
+                return dp[row][col] = cal(row, col - 1, dp);
+            }
+            if (col == 0)
+            {
+                return dp[row][col] = cal(row - 1, col, dp);
+            }
+
+            return dp[row][col] = cal(row - 1, col, dp) + cal(row, col - 1, dp);
         }
-        int ans=0;
-        if(n-2>=0)ans+=dp[n-2][m-1];
-        if(m-2>=0)ans+=dp[n-1][m-2];
-        return max(ans,1);
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return cal(m-1,n-1,dp);
         
     }
 };
