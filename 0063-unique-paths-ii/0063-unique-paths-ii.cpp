7b5a8d1535a25b6166812ad48c5fbc7c8820002f
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-10-09 22:39:39 +0530

    Time: 5 ms (40.66%), Space: 8.1 MB (7.04%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,33 +1,26 @@
 class Solution
 {
+    
+    int cal(int row,int col,vector<vector<int>>&v,vector<vector<int>>&dp){
+            if(row==0&&col==row){
+                return dp[row][col]=1;
+            }
+            if(row<0||col<0)return 0; //overFlow
+            if(v[row][col]==1)return dp[row][col]=0;//obstacle
+            if(dp[row][col]!=-1)return dp[row][col];//memoize
+            
+            return dp[row][col]=cal(row-1,col,v,dp)+cal(row,col-1,v,dp);
+    }
+    
     public:
         int uniquePathsWithObstacles(vector<vector < int>> &v)
         {
             int n = v.size(), m = v[0].size();
-
-            vector<vector<int>>dp(n,vector<int>(m,0));
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    if (i == 0 && j == 0)
-                    {
-                        if (v[i][j] == 1) dp[i][j] = 0;
-                        else dp[i][j] = 1;
-                    }
-                    else
-                    {
-                        if (i - 1 >= 0 && v[i - 1][j] != 1&&v[i][j]!=1)
-                        {
-                            dp[i][j] += dp[i - 1][j];
-                        }
-                        if (j - 1 >= 0 && v[i][j - 1] != 1&&v[i][j]!=1)
-                        {
-                            dp[i][j] += dp[i][j - 1];
-                        }
-                    }
-                }
-            }
-            return dp[n-1][m-1];
+            if(v[0][0]==1||v[n-1][m-1]==1)return 0;
+            
+            vector<vector<int>>dp(n,vector<int>(m,-1));
+            
+            return cal(n-1,m-1,v,dp);
+            
         }
 };
