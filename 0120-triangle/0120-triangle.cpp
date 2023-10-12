b4b447a97c58e869541011a9d58c1a90c0c2f6a3
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-10-12 19:06:59 +0530

    Time: 8 ms (29.17%), Space: 9.5 MB (5.14%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,41 +1,39 @@
-class Solution {
-public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size();
-        vector<vector<int>>dp(n);
-        
-        for(int i=0;i<n;i++)
+class Solution
+{
+    public:
+
+        int cal(int prev, int row, vector<vector < int>> &tr, vector< vector< int>> &dp)
         {
-            for(int j=0;j<triangle[i].size();j++)
+            if (prev < 0||prev>=tr[row].size()) return INT_MAX;
+            int n = tr.size();
+            if (row == 0)
             {
-                if(i==0&&j==0)
-                {
-                    dp[i].push_back(triangle[i][j]);
-                }
-                else
-                {
-                    int same=10000 ,prev=10000;
-                    if(j-1>=0)
-                    {
-                        prev=dp[i-1][j-1];
-                    }
-                    if(j+1<=triangle[i].size()-1)
-                    {
-                        same=dp[i-1][j];
-                    }
-                    int val=triangle[i][j]+min(same,prev);
-                    dp[i].push_back(val);
-                    
-                }
+                return dp[row][0]= tr[row][0];
             }
-        }
-        
-        int ans=INT_MAX;
-        for(int j=0;j<triangle[n-1].size();j++)
-        {
-            ans=min(ans,dp[n-1][j]);
+            if (dp[row][prev] != INT_MAX) return dp[row][prev];
+
+            int mini = INT_MAX;
+       
+                int curr = tr[row][prev] + min(cal(prev, row - 1, tr, dp), cal(prev - 1, row - 1, tr, dp));
+                mini = min(mini, curr);
+            // cout<<row<<" "<<mini<<endl;
+            
+            return dp[row][prev] = mini;
         }
 
-        return ans;
+    int minimumTotal(vector<vector < int>> &tr)
+    {
+
+        int n = tr.size();
+        if(n==1)return tr[0][0];
+        vector<vector < int>> dp(n, vector<int> (n+1 , INT_MAX));
+        int mini=INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+          int curr=cal(i, n-1, tr, dp);
+            mini=min(mini,curr);
+        }
+        return mini;
+       
     }
 };
