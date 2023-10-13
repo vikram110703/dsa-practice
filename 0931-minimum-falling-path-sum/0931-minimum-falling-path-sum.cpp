3adf11a1f0c19f9aea131d16412de60d456d6678
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-10-13 10:18:23 +0530

    Time: 16 ms (24.27%), Space: 11 MB (25.89%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,28 +1,66 @@
-class Solution {
-public:
-    int minFallingPathSum(vector<vector<int>>& mat) {
-        int n=mat.size();
-        vector<vector<int>>dp(n,vector<int>(n,0));
-        for(int i=0;i<n;i++)
+class Solution
+{
+    public:
+
+        int cal(int row, int ind, vector<vector < int>> &matrix, vector< vector< int>> &dp)
         {
-            
-            for(int j=0;j<n;j++)
+            int n = matrix.size();
+            if (ind < 0 || ind >= n) return INT_MAX;
+
+            if (row == n - 1)
             {
-                if(i==0)
-                {
-                    dp[i][j]=mat[i][j];
-                }
-                else
-                {
-                    int same=INT_MAX,prev=INT_MAX,next=INT_MAX;
-                    same=dp[i-1][j];
-                    if(j-1>=0)prev=dp[i-1][j-1];
-                    if(j+1<=n-1)next=dp[i-1][j+1];
-                    dp[i][j]=mat[i][j]+min({same,prev,next});
-                }
+                return dp[row][ind]= matrix[row][ind];
+            }
+
+            if (dp[row][ind] != -1) return dp[row][ind];
+
+            return dp[row][ind] = matrix[row][ind] + min({ cal(row + 1, ind, matrix, dp),
+                cal(row + 1, ind , matrix, dp),
+                cal(row + 1, ind - 1, matrix, dp) });
+
+        }
+
+    int minFallingPathSum(vector<vector < int>> &matrix)
+    {
+        //MEMOIZATION -> TLE 
+        
+        
+//         int n = matrix.size();
+//         vector<vector < int>> dp(n, vector<int> (n, -1));
+
+//         int mini = INT_MAX;
+//         for (int j = 0; j < n; j++)
+//         {
+//             mini = min(mini, cal(0, j, matrix, dp));
+//         }
+
+//         return mini;
+        
+        // TABULATION ....
+        
+        int n=matrix.size();
+        vector<vector<int>>dp(n,vector<int>(n,INT_MAX));
+        
+        for(int j=0;j<n;j++)dp[0][j]=matrix[0][j];// base case 
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                int mini=INT_MAX;
+                if(j-1>=0)mini=min(mini,dp[i-1][j-1]);
+                if(j+1<n)mini=min(mini,dp[i-1][j+1]);
+                mini=min(mini,dp[i-1][j]);
+                
+                dp[i][j]=matrix[i][j]+mini;
+                
             }
         }
+        
         return *min_element(dp[n-1].begin(),dp[n-1].end());
+        
+        
+        
+        
+        
+        
         
     }
 };
