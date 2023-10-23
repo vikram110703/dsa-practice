f3b8b92fa12bcf1d09552a493d11eb34bae12ee1
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-10-23 23:37:07 +0530

    Time: 207 ms (32.46%), Space: 131.3 MB (77.30%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+     
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+       
+        for(int i=0;i<n;i++){
+            if(i==0)nums[i]=nums[i];
+            else{
+                nums[i]+=nums[i-1];
+            }
+        }
+ 
+        unordered_map<int,int>freq;
+        freq[0]=-1;
+        for(int i=0;i<n;i++){
+            if(freq.count(nums[i]%k)){
+                if(i-freq[nums[i]%k]>=2)return true;
+            }
+            else
+            freq[nums[i]%k]=i;
+        }
+      
+        // }
+        return false;
+        
+        
+        
+    }
+};
