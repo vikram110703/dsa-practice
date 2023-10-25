293c92ab736d3ef24f2f05537e71499eb9cfc25d
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-10-25 23:19:11 +0530

    Time: 274 ms (45.87%), Space: 179.6 MB (11.13%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    bool cal(int curr,int ind,vector<int>&nums,vector<vector<int>>&dp)
-    {
-        int n=nums.size();
-        if(dp[ind][curr]!=-1)return dp[ind][curr];
-        if(ind>=n){
-            if(curr==0)return 1;
-            return 0;
+    int cal(int ind,int reqSum,vector<int>&nums,vector<vector<int>>&dp){
+        if(dp[ind][reqSum]!=-1)return dp[ind][reqSum];
+        if(ind==nums.size()){
+            if(reqSum==0)return 1;
+            else return 0;
         }
         
-        bool take=false,notTake=false;
-        if(curr>=nums[ind])
-        take|=cal(curr-nums[ind],ind+1,nums,dp);
-        notTake|=cal(curr,ind+1,nums,dp);
-        bool res=take|notTake;
-        if(res==true){
-            dp[ind][curr]=1;
-        }
-        else dp[ind][curr]=0;
-        return dp[ind][curr];
+        bool take=0,notTake=0;
+        if(nums[ind]<=reqSum)take=cal(ind+1,reqSum-nums[ind],nums,dp);
+        notTake=cal(ind+1,reqSum,nums,dp);
+        return dp[ind][reqSum]=take|notTake;
+        
     }
     
     bool canPartition(vector<int>& nums) {
@@ -27,10 +20,10 @@ public:
         for(int i=0;i<n;i++){
             sum+=nums[i];
         }
-        if(sum&1==1)return false;
-        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        return cal(sum/2,0,nums,dp);
-    
+        if((sum&1)==1)return false;
+        vector<vector<int>>dp(n+1,vector<int>(sum,-1));
+        int ans=cal(0,sum/2,nums,dp);
+        return ans;
         
     }
 };
