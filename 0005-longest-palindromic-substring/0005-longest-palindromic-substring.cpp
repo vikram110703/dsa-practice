91102ac2eb7b71e5f66add555c010f7fa7b11772
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-10-27 15:39:27 +0530

    Time: 532 ms (23.66%), Space: 294 MB (12.03%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,52 +1,38 @@
-class Solution
-{
-    public:
-        string longestPalindrome(string s)
-        {
-
-            int n = s.size();
-            int arr[n][n];
-            int maxi = 1, strtIndx = 0, endIndx = 0;
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    arr[i][j] = 0;
-                    if (i == j) arr[i][j] = 1;
-                    if (j - i == 1)
-                    {
-                        if (s[i] == s[j])
-                        {
-                            arr[i][j] = 2;
-                            maxi = 2;
-                            strtIndx = i, endIndx = j;
-                        }
-                    }
-                }
-            }
-
-            for (int gap = 2; gap < n; gap++)
-            {
-                for (int i = 0; i < n; i++)
-                {
-                    int j = i + gap;
-                    if (j >= n) break;
-                    if (arr[i + 1][j - 1] > 0 && (s[i] == s[j]))
-                    {
-                        arr[i][j] = gap;
-                        maxi = max(maxi, arr[i][j]);
-                        if (maxi == arr[i][j])
-                        {
-                            strtIndx = i, endIndx = j;
-                        }
-                    }
-                    else
-                    {
-                        arr[i][j] = 0;
-                    }
-                }
-            }
-
-            return s.substr(strtIndx, endIndx - strtIndx + 1);
+class Solution {
+public:
+    int cal(int strt,int end,string&s,vector<vector<int>>&dp){
+            int n=s.size();
+            if(end<strt||strt>=n||end<0||strt<0||end>=n)return 0;
+        if(dp[strt][end]!=-1)return dp[strt][end];
+        if(strt==end)return dp[strt][end]=1;
+        if(s[strt]!=s[end])return dp[strt][end]=0;
+        else{
+            //s[strt]==s[end]
+            if(end-strt==1)return dp[strt][end]=1;
+            bool isPal=cal(strt+1,end-1,s,dp);
+            return dp[strt][end]=isPal;
         }
+        
+        
+    }
+    
+    //this is memoization version     
+    
+    string longestPalindrome(string s) {
+        int n=s.size();
+        // int maxLen=1;
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        string palindrome;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int ans=cal(i,j,s,dp);
+                if(ans==1)
+                {
+                    if(palindrome.size()<j-i+1)palindrome=s.substr(i,j-i+1);
+                }
+                // if(ans==1)cout<<i<<" "<<j<<endl;
+            }
+        }
+        return palindrome;
+    }
 };
