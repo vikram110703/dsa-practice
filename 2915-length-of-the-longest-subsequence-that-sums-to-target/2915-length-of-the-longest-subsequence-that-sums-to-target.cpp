98b1c1b6922506d0f1ada143b02ee6b5f50fdf06
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-10-28 23:15:23 +0530

    Time: 541 ms (8.33%), Space: 153.3 MB (33.33%) - LeetHub

---
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    int cal(int ind,int sum,vector<int>&nums,vector<vector<int>>&dp)
+    {        
+        if(ind==nums.size()){ 
+            if(sum==0)return 0;
+            else return -1e5;
+        }
+        
+        if(dp[ind][sum]!=-1)return dp[ind][sum];
+        int take=-1e5,notTake=-1e5;
+        
+        if(nums[ind]<=sum){
+            take=1+cal(ind+1,sum-nums[ind],nums,dp);
+        }
+        notTake=cal(ind+1,sum,nums,dp);
+        
+        return dp[ind][sum]=max(take,notTake);
+        
+    }
+    
+    
+    
+    int lengthOfLongestSubsequence(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
+        int ans=cal(0,k,nums,dp);
+
+        if(ans<=0)return -1;
+        else return ans;
+        
+        
+        
+  
+        
+        
+    }
+};
+
