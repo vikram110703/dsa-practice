1b4c1692dd0e7ff7c6cb60829e6ec8e067e63262
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-12-08 08:59:36 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (92.41%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,63 +1,16 @@
-class Solution
-{
-    public:
-        string gcdOfStrings(string str1, string str2)
-        {
-            int n = str1.size();
-            int m = str2.size();
-            map<string, int> a, b;
-            for (int i = 0; i < n; i++)
-            {
-                string substr = str1.substr(0, i + 1);
-                a[substr]++;
-                for (int j = i + 1; j < n;j+=i+1 )
-                {
-                    if (j + i <= n - 1)
-                    {
-                        string ss = str1.substr(j, i + 1);
-                        if(substr==ss)a[substr]++;
-                    }
-                    
-                }
-            }
-
-            int maxi = 0;
-            string ans = "";
-           for (int i = 0; i < m; i++)
-            {
-                string substr = str2.substr(0, i + 1);
-               b[substr]++;
-                for (int j = i + 1; j < m; j+=i+1)
-                {
-                    if (j + i <= m - 1)
-                    {
-                        string ss = str2.substr(j, i + 1);
-                        if(substr==ss)b[substr]++;
-                    }
-                }
-            }
-            // for(auto it:a){
-            //     cout<<it.first<<" "<<it.second<<endl;
-            // }
-            for (auto &it: a)
-            {
-                string ss = it.first;
-                int cnt = it.second, sz = ss.size();
-                if (cnt *sz == n)
-                {
-                    if (b.count(ss))
-                    {
-                        if (sz *b[ss] == m)
-                        {
-                            if (sz > maxi)
-                            {
-                                maxi = sz;
-                                ans = ss;
-                            }
-                        }
-                    }
-                }
-            }
-            return ans;
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+     int n=str1.size(),m=str2.size();
+        int gcd=__gcd(n,m);
+        string subString=str1.substr(0,gcd);
+        for(int i=0;i<n;i+=gcd){
+            if(str1.substr(i,gcd)!=subString)return "";
         }
+        for(int i=0;i<m;i+=gcd){
+            if(str2.substr(i,gcd)!=subString)return "";
+        }
+        return subString ;
+        
+    }
 };
