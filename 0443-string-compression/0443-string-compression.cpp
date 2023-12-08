1fb0246ca61c980cd866343a2844742689088cdb
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-12-08 10:37:53 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (75.33%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,47 +1,21 @@
-class Solution
-{
-    public:
-        int compress(vector<char> &chars)
-        {
-            int n = chars.size();
-            int j = 0;
-            for (int i = 0; i < n; i++)
-            {
-                int cnt = 0;
-                char a = chars[i];
-                while (i + 1 < n && (chars[i] == chars[i + 1]))
-                {
-                    cnt += 1;
-                    i += 1;
-                }
-                cnt += 1;
-                chars[j++] = a;
-                // cout << a << " cnt -> " << cnt << endl;
-
-                if (cnt < 10)
-                {
-                    if (cnt > 1)
-                    {
-                        
-                        chars[j] = (cnt + '0');
-                        j+=1;
-                    }
-                }
-                else
-                {
-                   
-                    string s = to_string(cnt);
-                    for (int k = 0; k < s.size(); k++)
-                    {
-                        chars[j] = s[k];
-                        j += 1;
-                    }
-                }
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+      string ans="";
+        for(int i=0;i<chars.size();i++){
+            int cnt=0;
+            char curr=chars[i];
+            while(i<chars.size()&&chars[i]==curr){
+                cnt+=1;i+=1;
             }
-            while (chars.size() > j)
-            {
-                chars.pop_back();
-            }
-            return j;
+            ans+=curr;
+            if(cnt>1)ans+=(to_string(cnt));
+            if(i==chars.size())break;
+            else i-=1;
         }
+        for(int i=0;i<ans.size();i++){
+            chars[i]=ans[i];
+        }
+        return ans.size();
+    }
 };
