93d1a1c49a5ceb32c7b1828c7e73f8f8b4ea4348
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-12-14 22:20:10 +0530

    Time: 259 ms (87.17%), Space: 111.1 MB (66.06%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,36 +1,31 @@
 class Solution
 {
     public:
-
-        void dfs(int node,  map<int, vector < pair<int,int>>> &adj, map< int, bool> &viss, int &ans)
-        {
-            viss[node] = true;
-            for (auto it: adj[node])//adj[node]-> vector<pair>
-            {
-                if (viss[it.first] == false)
-                {
-                    if(it.second==1)ans+=1;// 1-> actual node which is in forward dir so ans+=1
-                    dfs(it.first, adj, viss, ans);
-                }
+    vector<vector<pair<int,int>>>adj;
+    vector<bool>viss;
+    int ans=0;
+    void dfs(int node){
+        viss[node]=true;
+        for(auto &it:adj[node]){
+            if(viss[it.first]==false){
+                ans+=it.second;
+                dfs(it.first);
             }
         }
+    }
 
     int minReorder(int n, vector<vector < int>> &connections)
     {
-        map<int, bool> viss;
-        int ans = 0;
-        map<int, vector < pair<int,int>>> adj;
-        for (int i = 0; i < connections.size(); i++)
-        {
-            int a = connections[i][0];
-            int b = connections[i][1];
-            //1-> actual path
+        int nn=connections.size();
+        adj.resize(n);
+        viss.resize(n,false);
+        for(int i=0;i<nn;i++){
+            int a=connections[i][0];
+            int b=connections[i][1];
             adj[a].push_back({b,1});
-            //-1 imaginary path , to travel in tree
-            adj[b].push_back({a,-1});
+            adj[b].push_back({a,0});//img edge
         }
-
-            dfs(0,adj,viss,ans);
+        dfs(0);
         return ans;
     }
 };
