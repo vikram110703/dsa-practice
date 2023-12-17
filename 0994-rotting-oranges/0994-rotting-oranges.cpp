38f8747a402bc1b6d9ca97acc1d4115d4a66749d
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-12-17 10:13:24 +0530

    Time: 7 ms (46.28%), Space: 14.2 MB (12.84%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,69 +1,52 @@
 class Solution {
 public:
+    
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size(),m=grid[0].size();
-        
-        vector<vector<bool>>viss(n,vector<bool>(m,false));
-        queue<pair<pair<int,int>,int>>q;   //{{i,j},time}
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]==2)
-                {
-                    q.push({{i,j},0});
-                    viss[i][j]=true;
+       queue<pair<int,int>>q;
+        map<pair<int,int>,bool>viss;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                viss[{i,j}]=false;
+                if(grid[i][j]==2){
+                    q.push({i,j});
                 }
             }
         }
-        int minTime=0;
-        
-        while(!q.empty())
-        {
-            int i=q.front().first.first;
-            int j=q.front().first.second;
-            int time=q.front().second;
-            minTime=max(minTime,time);
+        int time=0;
+        while(!q.empty()){
+            int sz=q.size();
+            while(sz>0){
+                sz-=1;
+            int i=q.front().first;
+            int j=q.front().second;
+                // cout<<i<<" "<<j<<endl;
+            viss[{i,j}]=true;
             q.pop();
-            
-            if(i-1>=0&&viss[i-1][j]==false&&grid[i-1][j]==1)
-            {
-                q.push({{i-1,j},time+1});
-                viss[i-1][j]=true;
+            if(i+1<n&&viss[{i+1,j}]==false)if(grid[i+1][j]==1)
+            {q.push({i+1,j});  viss[{i+1,j}]=true;
             }
-             if(j-1>=0&&viss[i][j-1]==false&&grid[i][j-1]==1)
-            {
-                q.push({{i,j-1},time+1});
-                viss[i][j-1]=true;
+            if(i-1>=0&&viss[{i-1,j}]==false)if(grid[i-1][j]==1){
+                q.push({i-1,j});viss[{i-1,j}]=true;
             }
-             if(i+1<n&&viss[i+1][j]==false&&grid[i+1][j]==1)
-            {
-                q.push({{i+1,j},time+1});
-                viss[i+1][j]=true;
+            if(j+1<m&&viss[{i,j+1}]==false)if(grid[i][j+1]==1)
+            {q.push({i,j+1}); viss[{i,j+1}]=true;
             }
-              if(j+1<m&&viss[i][j+1]==false&&grid[i][j+1]==1)
-            {
-                q.push({{i,j+1},time+1});
-                viss[i][j+1]=true;
+            if(j-1>=0&&viss[{i,j-1}]==false)if(grid[i][j-1]==1)
+            {q.push({i,j-1}); viss[{i,j-1}]=true;
             }
-            
         }
-        bool chk=true;
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]==1&&viss[i][j]==false)
-                {
-                    chk=false;
-                    break;
+          if(q.size()>0)time+=1;
+            // cout<<endl;
+        }
+           for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1&&viss[{i,j}]==false){
+                    return -1;
                 }
             }
         }
-        
-       return chk==true? minTime: -1;
-        
+        return time;
         
     }
 };
