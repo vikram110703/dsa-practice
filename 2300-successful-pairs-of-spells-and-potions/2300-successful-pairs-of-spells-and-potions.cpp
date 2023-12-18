b6b2f5f01904397ec931b1d12a87318e129be93b
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-12-18 07:38:38 +0530

    Time: 192 ms (22.02%), Space: 97.5 MB (87.48%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    vector<int> successfulPairs(vector<int>& s, vector<int>& p, long long t) {
-        
-        int n=s.size();
-        int m=p.size();;
-        sort(p.begin(),p.end());
-        // multiset<long long>st;
-        // for(int i=0;i<m;i++)
-        // {
-        //     st.insert(p[i]*1ll);
-        // }
-        vector<int>ans;
-        for(int i=0;i<n;i++)
-        {
-            long long  req=(t/s[i]*1ll);
-            if(t%(s[i]*1ll)==0){;;}
-            else req+=1;
-            auto it=lower_bound(p.begin(),p.end(),req);
-            int total=distance(it,p.end());
-            ans.push_back(total);
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int n=spells.size(),m=potions.size();
+        sort(potions.begin(),potions.end());
+        for(int i=0;i<n;i++){
+            int curr=spells[i];
+            long long req=(success/curr);
+            if(success%curr>0)req+=1;
+            auto it=lower_bound(potions.begin(),potions.end(),req)-potions.begin();
+            int count=m-it;
+            spells[i]=count;
         }
-        return ans;
+        return spells;
+        
     }
 };
