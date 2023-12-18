d21019c3fde61fd04c63a2b9d7a4fabf206ff3a5
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-12-18 08:08:07 +0530

    Time: 44 ms (26.89%), Space: 19.4 MB (35.58%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,65 +1,24 @@
-class Solution
-{
-    public:
-        int minEatingSpeed(vector<int> &v, int h)
-        {
-            sort(v.begin(), v.end());
-            int n = v.size();
-            int strt, end, mid;
-            long long sum = 0;
-            for (int i = 0; i < n; i++)
-            {
-                sum += (v[i] *1ll);
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n=piles.size();
+        int strt=1,end=INT_MAX,mid;
+        while(end-strt>1){
+            mid=strt+(end-strt)/2;
+            int timeTaken=0;
+            for(int i=0;i<n;i++){
+                timeTaken+=(piles[i]/mid);
+                if(piles[i]%mid>0)timeTaken+=1;
             }
-            strt = (sum / (h *1ll));
-            strt *= 1;
-           	//can't eat >max element 
-            end = *max_element(v.begin(), v.end());
-
-           	// cout<<" strt "<<strt<<" end "<<end<<endl;
-            int reqTime = 0;
-            while (end - strt > 1)
-            {
-                mid = strt + (end - strt) / 2;
-               	// cout<<" mid "<<mid<<" ";
-                reqTime = 0;
-                for (int i = 0; i < n; i++)
-                {
-                    if (v[i] % mid == 0)
-                    {
-                        reqTime += v[i] / mid;
-                    }
-                    else
-                    {
-                        reqTime += (v[i] / mid) + 1;
-                    }
-                }
-                if (reqTime > h)
-                {
-                    strt = mid + 1;
-                }
-                else
-                {
-                    end = mid;
-                }
-            }
-            reqTime = 0;
-            if (strt == 0) return end;
-        //chk if he eat mid banana per hour ,than how much time take to eat all piles...
-            for (int i = 0; i < n; i++)
-            {
-                if (v[i] % strt == 0)
-                {
-                    reqTime += v[i] / strt;
-                }
-                else
-                {
-                    reqTime += (v[i] / strt) + 1;
-                }
-            }
-            if (reqTime <= h)
-                return strt;
-            else
-                return end;
+            if(timeTaken>h)strt=mid+1;
+            else end=mid;
         }
+      int timeTaken=0;
+            for(int i=0;i<n;i++){
+                timeTaken+=(piles[i]/strt);
+                if(piles[i]%strt>0)timeTaken+=1;
+            }
+        if(timeTaken<=h)return strt;
+        return end;
+    }
 };
