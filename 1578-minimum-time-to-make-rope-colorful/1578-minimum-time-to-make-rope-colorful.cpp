644585658abd5ea285be8b257c1b93625a254068
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2023-12-27 11:14:25 +0530

    Time: 374 ms (5.07%), Space: 165.9 MB (5.07%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int prevTime;
-
-    
-    int cal(int i ,int prev,vector<int>&neededTime,string &colors,int prevTime){
-        if(i>=neededTime.size()) return dp[i][prev]= 0;
-        if(dp[i][prev]!=-1)return dp[i][prev];
-        int ans=INT_MAX;
-        if(prev==colors[i]-'a'){
-     ans=min(prevTime,neededTime[i])+cal(i+1,prev,neededTime,colors,max(prevTime,neededTime[i]));
-        }
-        else{
-          int op3=cal(i+1,colors[i]-'a',neededTime,colors,neededTime[i]);
-            ans=min(ans,op3);
-        }
-       return dp[i][prev]=ans;
-        
-    }
-
+    multiset<int>st;
+    int ans=0;
     int minCost(string colors, vector<int>& neededTime) {
         int n=colors.size();
-        dp.resize(n+1,vector<int>(27,-1));
-        prevTime=neededTime[0];
-       int ans=cal(0,26,neededTime,colors,prevTime);
-        
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<27;j++){
-        //         cout<<dp[i][j]<<" ";
-        //     }cout<<endl;
-        // }
-        
-        return ans;
-       
+        char prev=colors[0];
+        for(int i=0;i<n;i++){
+            st.clear();
+            while(i<n&&colors[i]==prev){
+                st.insert(neededTime[i]);
+                i+=1;
+            }
+            while(st.size()>1){
+                ans+=(*st.begin());
+                st.erase(st.begin());
+            }
+            if(i<n){
+                prev=colors[i];
+                i-=1;
+            }
+        }
+       return ans;
     }
 };
