ef3320b6186eb78b7dec44c8007f0f71387fcdb8
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2024-01-13 08:59:44 +0530

    Time: 43 ms (92.21%), Space: 16.9 MB (82.29%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        multiset<int>st;
-        for(int i=0;i<t.size();i++)st.insert(s[i]);
+        vector<int>v(26,0);
+        for(int i=0;i<t.size();i++)v[s[i]-'a']++;
         for(int i=0;i<t.size();i++){
-            auto it=st.lower_bound(t[i]);
-            if(*it==t[i])st.erase(it);
+            v[t[i]-'a']--;
         }
-        return st.size();
+        int cnt=0;
+        for(int i=0;i<26;i++)if(v[i]>0)cnt+=v[i];
+        return cnt;
     }
 };
