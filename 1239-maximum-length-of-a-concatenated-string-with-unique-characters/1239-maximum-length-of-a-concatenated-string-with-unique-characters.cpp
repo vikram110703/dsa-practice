aa1498a87f09af8a20aadbf137da2fd939a4902a
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2024-01-23 09:08:43 +0530

    Time: 2944 ms (5.07%), Space: 453.3 MB (5.07%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,45 +1,27 @@
 class Solution {
 public:
-    vector<int>dp;
-    int n;
-    // int maxLen=0;
-    // map<char,int>mpp;
-    
-     int cal(int i,vector<string>&arr,int maxLen,map<char,int>&mpp){
-         if(i==n){
-             return maxLen;
-         }
-       //  if(dp[i]!=-1)return dp[i];
-         string s=arr[i];
-         bool chk=true;
-         for(int j=0;j<s.size();j++){
-             mpp[s[j]]++;
-             if(mpp[s[j]]>1){
-                 chk=false;
-             }
-         }
-         int take=0,notTake=0;
-         
-         if(chk){
-             take=cal(i+1,arr,maxLen+s.size(),mpp);
-            
-         }
-              for(int j=0;j<s.size();j++){
-                 mpp[s[j]]--;
-             }
-             notTake=cal(i+1,arr,maxLen,mpp);
-         
-         return dp[i]=max(take,notTake);
-     }
-    
     int maxLength(vector<string>& arr) {
-        n=arr.size();
-        dp.resize(n+1,-1);
-        map<char,int>mpp;
-        int ans= cal(0,arr,0,mpp);
-        for(int i=0;i<dp.size();i++){
-            cout<<dp[i]<<" ";
+        int n = arr.size();set<int> s;
+        int maxLen = 0;
+        
+        for(int i = 0;i<=(1<<n)-1;i++){
+            int len  = 0;
+            s.clear();
+            for(int j  =0;j<17;j++){
+                if(i & (1<<j)){
+                    for(int k = 0;k<arr[j].size();k++){
+                        if(s.count(arr[j][k])){
+                            len = 0;
+                            break;
+                        }
+                        s.insert(arr[j][k]);
+                        len++;
+                    }
+                }
+            }
+            maxLen = max(maxLen, len);
         }
-        return ans;
+        
+        return maxLen;
     }
 };
