66cf5137987fd6aafa05e88a7e06fba842ab2206
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2024-01-26 20:17:29 +0530

    Time: 48 ms (62.88%), Space: 41.9 MB (37.12%) - LeetHub

--- 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.cpp
+++ 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.cpp
@@ -1,54 +1,21 @@
-class Solution
-{
-    public:
-        int mod = 1e9 + 7;
-    
-    int kadan(vector<int>arr){
-         long long maxi = 0, curr = 0;
-        int n = arr.size();
-        for (int i = 0; i < n; i++)
-        {
-            curr = (curr + arr[i]) % mod;
-            maxi = max(maxi, curr);
-            if (curr <= 0)
-            {
-                curr = 0;
-            }
-        }
-        return maxi%mod;
-    } 
-    
-    
-    int kConcatenationMaxSum(vector<int> &arr, int k)
-    {
-        int n = arr.size();
-        long long maxiii;
-        maxiii= kadan(arr);
-            
-        if (k == 1)
-        {
-            return maxiii;
-        }
-        // if(k==2){
-        //     for(int i=0;i<n;i++)arr.push_back(arr[i]);
-        //     return kadan(arr);
-        // }
-        long long pre=0,suff=0,cur=0,curr=0;
-        for(int i=0;i<n;i++){
+class Solution {
+public:
+    int kConcatenationMaxSum(vector<int>& arr, int k) {
+        long long cur = 0 , maxSub = 0 , pre = 0, maxPre = 0 , suff = 0 , maxSuff = 0, n = arr.size() , mod = 1e9+7;
+        
+        for(int i = 0 ;i<n;i++){
             cur+=arr[i];
-            pre=max(pre,cur);
-            curr+=arr[n-1-i];
-            suff=max(suff,curr);
+            maxSub= max(maxSub, cur);
+            cur = max(cur,0ll);
+            
+            pre+=arr[i];
+            maxPre = max(pre,maxPre);
+            
+            suff+=arr[n-1-i];
+            maxSuff = max(maxSuff,suff);
         }
-         cout<<pre<<" "<<suff<<" "<<maxiii<<" "<<cur<<endl;
-        long long  total=pre+suff+(k-2)*cur;
-        return max({maxiii,0ll,total,(pre+suff)})%mod;
-       
+        
+        return k==1 ? maxSub : max({0ll, maxSub, maxPre + maxSuff + (k-2)*max(0ll,pre) }) % mod;
+        
     }
 };
-
-
-
-
-// [-5,-2,0,0,3,9,-2,-5,4,-5,-2,0,0,3,9,-2,-5,4]
-
