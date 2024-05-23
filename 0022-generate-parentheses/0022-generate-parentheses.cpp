202fca7700d61e21d91afe3d2af55aca333adc1b
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2024-05-23 08:51:29 +0530

    Time: 3 ms (63.72%), Space: 11.7 MB (85.25%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -3,7 +3,7 @@ public:
     // l->(
     // r->) 
     int nn;
-    void chk(int ind,int l,int r,vector<string>&ans,string s){
+    void chk(int ind,int l,int r,vector<string>&ans,string &s){
         if(ind>=2*nn){
             if(l==r)
             ans.push_back(s);
@@ -15,11 +15,13 @@ public:
             s.pop_back();
             s+=')';
             chk(ind+1,l,r+1,ans,s);
+            s.pop_back();
             
         }
         else if(l==r){
             s+='(';
             chk(ind+1,l+1,r,ans,s);
+            s.pop_back();
         }
         
     }
