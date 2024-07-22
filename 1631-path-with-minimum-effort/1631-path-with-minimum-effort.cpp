92370cf09962abab46f949dbbc7617455f11c6f8
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2024-07-22 11:41:10 +0530

    Time: 125 ms (25.27%), Space: 42.6 MB (14.78%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -3,52 +3,50 @@ public:
     int n;
     int m;
     void bfs(int ii,int jj,vector<vector<int>>&heights,vector<vector<int>>&dist){
-        queue<pair<int,int>>q;
-        q.push({ii,jj});
+        queue<pair<int,pair<int,int>>>q;
+        q.push({0,{ii,jj}});
         dist[ii][jj]=0;
         while(!q.empty()){
-            int i=q.front().first;
-            int j=q.front().second;
+            int currDist=q.front().first;
+            int i=q.front().second.first;
+            int j=q.front().second.second;
             // cout<<ii<<" "<<jj<<" "<<dist[ii][jj]<<endl;
             q.pop();
+            if(currDist>dist[i][j])continue;
             if(i+1<n){
                 int ChildDist=dist[i+1][j];
-                int currDist=dist[i][j];
                 int diff=abs(heights[i][j]-heights[i+1][j]);
                 int maxi=max(diff,currDist);
                 if(dist[i+1][j]>maxi){
                     dist[i+1][j]=maxi;
-                    q.push({i+1,j});
+                    q.push({maxi,{i+1,j}});
                 }
             }
             if(i-1>=0){
                 int ChildDist=dist[i-1][j];
-                int currDist=dist[i][j];
                 int diff=abs(heights[i][j]-heights[i-1][j]);
                 int maxi=max(diff,currDist);
                 if(dist[i-1][j]>maxi){
                     dist[i-1][j]=maxi;
-                    q.push({i-1,j});
+                    q.push({maxi,{i-1,j}});
                 }
             }
             if(j+1<m){
                 int ChildDist=dist[i][j+1];
-                int currDist=dist[i][j];
                 int diff=abs(heights[i][j]-heights[i][j+1]);
                 int maxi=max(diff,currDist);
                 if(dist[i][j+1]>maxi){
                     dist[i][j+1]=maxi;
-                    q.push({i,j+1});
+                    q.push({maxi,{i,j+1}});
                 }
             }
             if(j-1>=0){
                 int ChildDist=dist[i][j-1];
-                int currDist=dist[i][j];
                 int diff=abs(heights[i][j]-heights[i][j-1]);
                 int maxi=max(diff,currDist);
                 if(dist[i][j-1]>maxi){
                     dist[i][j-1]=maxi;
-                    q.push({i,j-1});
+                    q.push({maxi,{i,j-1}});
             }
         }
     }
@@ -59,11 +57,7 @@ public:
         m=heights[0].size();
         vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
         bfs(0,0,heights,dist);
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         cout<<dist[i][j]<<" ";
-        //     }cout<<endl;
-        // }
+
         return dist[n-1][m-1];
     }
 };
