0c7b73307beb4770108ec66745936c75ac5778f7
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2024-07-22 21:07:31 +0530

    Time: 22 ms (23.51%), Space: 17.1 MB (37.88%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,49 +1,56 @@
-class Solution
-{
-    public:
-        int findCheapestPrice(int n, vector<vector < int>> &flights, int src, int dst, int k)
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+       // Create the adjacency list to depict airports and flights in
+        // the form of a graph.
+        vector<pair<int, int>> adj[n];
+        for (auto it : flights)
         {
-            int nn = flights.size();
-            vector<vector<pair<int, int>>> adj(n);
-            for (int i = 0; i < nn; i++)
-            {
-               	// from[({to,price})]
-                adj[flights[i][0]].push_back({ flights[i][1],
-                    flights[i][2] });
-            }
-            vector<int> stops(n, INT_MAX);
-           	//{dist,{node,stops}} 
-            priority_queue<pair<int, pair<int, int>>, vector< pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-            pq.push({ 0,
-                {
-                    src,
-                    0
-                } });
-            while (!pq.empty())
-            {
-                pair<int, pair<int, int>> curr = pq.top();
-                pq.pop();
-                int node = curr.second.first;
-                int dist = curr.first;
-                int steps = curr.second.second;
-               	//           bcz of use of pq, if we get a node lesser steps then currnt then we will no include it again bcz we r getting nodes in ascending ordr of dist    
-                if (steps > stops[node] || steps > k + 1)
-                    continue;
+            adj[it[0]].push_back({it[1], it[2]});
+        }
 
-                stops[node] = steps;
-               	//return at 1st occurence bcz we r using pq to sort acrding to dist or price
-                if (node == dst) return dist;
-                for (auto &it: adj[node])
+        // Create a queue which stores the node and their distances from the
+        // source in the form of {stops, {node, dist}} with ‘stops’ indicating 
+        // the no. of nodes between src and current node.
+        queue<pair<int, pair<int, int>>> q;
+        
+        q.push({0, {src, 0}});
+
+        // Distance array to store the updated distances from the source.
+        vector<int> dist(n, 1e9);
+        dist[src] = 0;
+
+        // Iterate through the graph using a queue like in Dijkstra with 
+        // popping out the element with min stops first.
+        while (!q.empty())
+        {
+            auto it = q.front();
+            q.pop();
+            int stops = it.first;
+            int node = it.second.first;
+            int cost = it.second.second;
+
+            // We stop the process as soon as the limit for the stops reaches.
+            if (stops > K)
+                continue;
+            for (auto iter : adj[node])
+            {
+                int adjNode = iter.first;
+                int edW = iter.second;
+
+                // We only update the queue if the new calculated dist is
+                // less than the prev and the stops are also within limits.
+                if (cost + edW < dist[adjNode] && stops <= K)
                 {
-                    int price = it.second, child = it.first;
-                    pq.push({ dist + price,
-                        {
-                            child,
-                            steps + 1
-                        } });
+                    dist[adjNode] = cost + edW;
+                    q.push({stops + 1, {adjNode, cost + edW}});
                 }
             }
-           	// unable to reach dst
-            return -1;
         }
+        // If the destination node is unreachable return ‘-1’
+        // else return the calculated dist from src to dst.
+        if (dist[dst] == 1e9)
+            return -1;
+        return dist[dst]; 
+    }
 };
