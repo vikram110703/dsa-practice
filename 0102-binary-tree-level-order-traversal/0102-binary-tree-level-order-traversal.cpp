ed20f4ad02d1a1875432b39b1050917ce1ff34d6
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2024-07-28 16:12:57 +0530

    Time: 3 ms (82.22%), Space: 15.3 MB (12.70%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -12,31 +12,28 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        TreeNode* tmp=root;
-        if(tmp==NULL)return {};
-        queue<TreeNode*>q;
-        q.push(tmp);
-        q.push(NULL);
-        vector<vector<int>>v;
-        vector<int>vv;
-        while(!q.empty())
-        {
-            TreeNode* fnode=q.front();
-            q.pop();
-            if(fnode==NULL)
-            {
-                v.push_back(vv);
-                vv.clear();
-                if(!q.empty())q.push(NULL);
-                else break;
-            }
-            else
-            {
-                vv.push_back(fnode->val);
-               if(fnode->left) q.push(fnode->left);
-               if(fnode->right) q.push(fnode->right);
-            }
-        }
-        return v;
+        vector<vector<int>>ans;
+        levelOrder(root,ans);
+        return ans;
     }
+    
+    void levelOrder(TreeNode*root,vector<vector<int>>&ans)
+    {
+        if(root==nullptr)return;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            vector<int>tmp;
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                TreeNode* fnode=q.front();
+                q.pop();
+                tmp.push_back(fnode->val);
+                if(fnode->left)q.push(fnode->left);
+                if(fnode->right)q.push(fnode->right);
+            }
+            ans.push_back(tmp);
+        }
+    }
+    
 };
