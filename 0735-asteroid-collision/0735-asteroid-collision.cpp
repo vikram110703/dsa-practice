7274e65c38e9dbea4a14d430321e6764b13288f3
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2024-08-07 10:01:32 +0530

    Time: 8 ms (74.12%), Space: 20.8 MB (27.44%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,74 +1,31 @@
 class Solution {
 public:
-    vector<int>asteroidCollision(vector<int>& asteroids) {
-        stack<int>st;
-        stack<int>tmp;
-        int j=asteroids.size()-1;
-        int i=0,jj=j;
-        while(i<=j)
-        {
-            if(asteroids[i]<0)i+=1;
-            else break;
-        }
-        while(j>=i)
-        {
-            if(asteroids[j]>0)j-=1;
-            else break;
-        }
-        jj=j;
-        while(j>=i)
-        {
-            if(st.size()==0)
-            {
-                st.push(asteroids[j]);
-                j-=1;
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> stack;
+        
+        for (int asteroid : asteroids) {
+            bool alive = true;
+            while (!stack.empty() && asteroid < 0 && stack.top() > 0) {
+                if (stack.top() < -asteroid) {
+                    stack.pop();
+                    continue;
+                } else if (stack.top() == -asteroid) {
+                    stack.pop();
+                }
+                alive = false;
+                break;
             }
-            else
-            {
-                if(st.top()*asteroids[j]>0)
-                {
-                    st.push(asteroids[j]);
-                    j-=1;
-                }
-                else{
-                    while(j>=0&&st.size()>0&&(st.top()*asteroids[j])<0)
-                    {
-                        if(abs(st.top())==abs(asteroids[j])&&st.top()<0)
-                        {
-                            st.pop();
-                            j-=1;
-                        }
-                        else if(st.top()>0)
-                        {
-                            st.push(asteroids[j]);
-                            j-=1;
-                        }
-                        else if(st.top()<0)//<-
-                        {
-                            if(asteroids[j]>abs(st.top())){
-                                st.pop();
-                            }
-                            else{
-                                j-=1;
-                            }
-                        }
-                    }
-                }
+            if (alive) {
+                stack.push(asteroid);
             }
         }
-      
-        vector<int>ans;
-        i-=1;
-        while(i>=0)ans.push_back(asteroids[i--]);
-        reverse(ans.begin(),ans.end());
-        while(st.size()>0)
-        {
-            ans.push_back(st.top());
-            st.pop();
+        
+        vector<int> result(stack.size());
+        for (int i = stack.size() - 1; i >= 0; --i) {
+            result[i] = stack.top();
+            stack.pop();
         }
-        // reverse(ans.begin(),ans.end());
-        jj+=1;
-        while(jj<asteroids.size())ans.push_back(asteroids[jj++]);
-        return ans;
+        
+        return result;
     }
 };
