303654dff359bbf84265b7cec9baf3c4d9c17e67
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2024-09-25 11:38:13 +0530

    Time: 306 ms (86.25%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+-- select * from Employee e1
+select e1.name from Employee e1 
+inner join Employee e2 
+on e1.id = e2.managerId
+group by e1.id
+having count(e1.id) >= 5
