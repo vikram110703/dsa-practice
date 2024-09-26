b5295a76deeb51c5a512376bf3bcb91303b6ecd5
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2024-09-26 15:08:00 +0530

    Time: 1056 ms (51.89%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -1,6 +1,17 @@
-select s.product_id,s.year as first_year,s.quantity,s.price 
-from Sales s
-left join Sales ss
-on s.product_id = ss.product_id and s.year>ss.year
-where ss.year is Null 
--- group by s.product_id ;
+-- select s.product_id,s.year as first_year,s.quantity,s.price 
+-- from Sales s
+-- left join Sales ss
+-- on s.product_id = ss.product_id and s.year>ss.year
+-- where ss.year is Null 
+
+
+# Write your MySQL query statement below
+
+WITH CTE AS (
+    SELECT product_id, MIN(year) AS minyear FROM Sales 
+    GROUP BY product_id 
+)
+
+SELECT s.product_id, s.year AS first_year, s.quantity, s.price 
+FROM Sales s
+INNER JOIN CTE ON cte.product_id = s.product_id  AND s.year = cte.minyear; 
