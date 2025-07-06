93410a6d816c0b84fcd1aeeba80e1cc27394d522
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2025-07-06 15:31:40 +0530

    Time: 3 ms (41.11%), Space: 14.2 MB (38.47%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
-    void solve(int ind, int sum, vector<int>& tmp, vector<int>& arr,
-               set<vector<int>>& ans) {
-
-        if (sum < 0)
-            return;
-        if (ind >= arr.size() || sum == 0) {
-            if (sum == 0) {
-                ans.insert(tmp);
-            }
-            return;
-        }
-
-        solve(ind + 1, sum, tmp, arr, ans);
-        // take i freq of ind ...
-        for (int i = 1; i <= arr[ind]; i++) {
-            tmp.push_back(ind);
-            solve(ind + 1, sum -(ind*i), tmp, arr,ans);
-        }
-        // now pop_back all the pushed values
-        while (tmp.size() && tmp.back()==ind) {
-            tmp.pop_back();
-        }
-    }
+    vector<vector<int>>ans;
+    vector<int>tmp;
 
     vector<vector<int>> combinationSum2(vector<int>& v, int target) {
-
-        vector<int> tmp;
-        set<vector<int>> ans;
-        sort(v.begin(), v.end());
-        vector<int> arr(51, 0);
-        for (auto& val : v)
-            arr[val] += 1;
-        solve(0, target, tmp, arr, ans);
-        vector<vector<int>> res;
-        for (auto& val : ans)
-            res.push_back(val);
-        return res;
+        sort(v.begin(),v.end());
+        solve(v,0,target);
+        return ans;
     }
+
+    void solve(vector<int>&v,int strt,int target){
+        if(target==0){
+            ans.push_back(tmp);
+            return;
+        }
+        if(target<1 || strt>v.size())return;
+
+        for(int i=strt;i<v.size();i++){
+            if(i>strt && v[i]==v[i-1])continue; //skip duplicates
+            
+            tmp.push_back(v[i]);
+            solve(v,i+1,target-v[i]);
+
+            tmp.pop_back();//without  arr[i] element
+        }
+    }
+
 };
