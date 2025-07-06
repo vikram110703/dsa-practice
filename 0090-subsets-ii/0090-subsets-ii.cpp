3d8ede23b5c123ea4d624ae1ba251205ffdc3548
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2025-07-06 14:43:00 +0530

    Time: 1 ms (50.32%), Space: 10.5 MB (67.84%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-    void print(int ind,vector<int>&nums,vector<vector<int>>&ans,vector<int>&tmp)
-    {
-        if(ind>=nums.size())
-        {
-            ans.push_back(tmp);
-            return ;
-        }
-        //take 
-        tmp.push_back(nums[ind]);
-        print(ind+1,nums,ans,tmp);
-        tmp.pop_back();//backTracking
-        //not take
-        print(ind+1,nums,ans,tmp);
-        
-    }
-    
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
         vector<int>tmp;
-        print(0,nums,ans,tmp);
-        set<vector<int>>st;
-        for(int i=0;i<ans.size();i++)
-        {
-            sort(ans[i].begin(),ans[i].end());
-            st.insert(ans[i]);
-        }
-        ans.clear();
-        for(auto &it:st)
-        {
-            ans.push_back(it);
-        }
-        return  ans;  
+        solve(nums,ans,tmp,0);
+        return ans;
     }
+
+    void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>&tmp,int strt){
+        ans.push_back(tmp);
+        for(int i=strt;i<nums.size();i++){
+            if(i>strt && nums[i]==nums[i-1])continue;// skip duplicates
+            tmp.push_back(nums[i]);
+            solve(nums,ans,tmp,i+1);
+            //undo changes
+            tmp.pop_back();
+        }
+    }
+     
 };
