9609a39c1d7fce420500767336907982876115b1
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2025-07-27 11:43:50 +0530

    Time: 3 ms (17.83%), Space: 18 MB (20.24%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
@@ -5,27 +5,28 @@ class Solution:
         right=-1
         i=1
         while(i<len(nums)-1):
+            while(i<len(nums) and nums[i-1]==nums[i]):
+                i+=1
+            left=nums[i-1]
+
+            if(i==len(nums)):
+                break
+
+            while(i+1<len(nums) and nums[i]==nums[i+1]):
+                i+=1
+            if(i+1==len(nums)):
+                break
+            right=nums[i+1]
+            
             for j in range(i+1,len(nums)):
                 if(nums[i]!=nums[j]):
                     right=nums[j]
                     break
     
-            j=i-1
-            while(j>=0):
-                if(nums[i]!=nums[j]):
-                    left=nums[j]
-                    break
-                else:
-                    j-=1
-
-            # print(left,right)
             if(left!=-1 and right!=-1):
                 if((left>nums[i] and right>nums[i])or(left<nums[i] and right<nums[i])):
                     count+=1
 
-                    # Skip same values
-            while(i+1<len(nums) and nums[i+1]==nums[i]):
-                i=i+1
             i+=1
 
         return count
