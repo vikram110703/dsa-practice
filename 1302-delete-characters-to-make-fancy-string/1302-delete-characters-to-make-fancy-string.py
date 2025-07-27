be0c7567955ea88462630f006bfdf2778e2afa75
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2025-07-27 12:03:19 +0530

    Time: 436 ms (14.23%), Space: 19.7 MB (51.88%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
@@ -17,30 +17,6 @@ class Solution:
 
             if(count<=2):
                 tmp+=s[i]
-            if(count==2):
-                while(i+1<n and s[i]==s[i+1]):
-                    i+=1
-                    count=1
             i+=1
 
         return tmp
-
-
-# class Solution:
-#     def makeFancyString(self, s: str) -> str:
-
-#         prev = s[0]
-#         frequency = 1
-#         ans = s[0]
-
-#         for i in range(1, len(s)):
-#             if s[i] == prev:
-#                 frequency += 1
-#             else:
-#                 prev = s[i]
-#                 frequency = 1
-
-#             if frequency < 3:
-#                 ans += s[i]
-
-#         return ans
