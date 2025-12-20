08065fac7182f0cfb321c1bf548e0f25fdebaa10
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2025-12-20 12:51:35 +0530

    Time: 0 ms (100%), Space: 52.2 MB (87.78%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int cal(int ind,vector<int>&nums,vector<int>&dp){
-        int n=nums.size();
-        if(ind==n-1)return 1;
-        if(dp[ind]!=-1)return dp[ind];
-        dp[ind]=0;
-        for(int i=1;i<=nums[ind];i++){
-            int aa=cal(ind+i,nums,dp);
-            if(aa==1){dp[ind]=1;break;}
-        }
-        return dp[ind];
-    }
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n+1,-1);
-        int ans=cal(0,nums,dp);
-        return ans==1;
+        bool isPossible=false;
+        int currMax=0;
+        int i=0;
+        while(i<n){
+            currMax=max(currMax,nums[i]);
+            if(currMax>0){
+            currMax--;
+            i++;
+            }
+            else break;
+        }
+       return i>=n-1;
     }
 };
