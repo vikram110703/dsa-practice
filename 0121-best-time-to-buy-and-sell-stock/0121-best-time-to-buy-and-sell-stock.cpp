d53c69f307e40e9b8656b5c812ce976646fef515
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2025-12-20 11:18:55 +0530

    Time: 215 ms (7.03%), Space: 168.9 MB (5.34%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
+    //if we sell , we will add money to our ans
+    //if we buy, we will decrese the money to our ans
+    int solve(int k,int ind,bool buy,vector<int>&prices,vector<vector<int>>&dp){
+        if(ind>=prices.size() || k<=0)return 0;
+        if(dp[ind][buy]!=-1)return dp[ind][buy];
+
+        int op1=0,op2=0;
+        if(buy){
+            op1=solve(k,ind+1,buy,prices,dp);
+            op2=-prices[ind]+solve(k,ind+1,!buy,prices,dp);
+        }
+        else{//sell
+            op1=solve(k,ind+1,buy,prices,dp);
+            op2=prices[ind]+solve(k-1,ind+1,!buy,prices,dp);
+        }
+        return dp[ind][buy]=max(op1,op2);
+    }
+
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<int>suff(n,0);
-        for(int i=n-1;i>=0;i--)
-        {
-            if(i==n-1){
-                suff[i]=prices[i];
-            }
-            else{
-               suff[i]= max(prices[i],suff[i+1]);
-            }
-        }
-        int ans=0;
-        for(int i=0;i<n-1;i++)
-        {
-            ans=max(ans,-prices[i]+suff[i+1]);
-            // cout<<suff[i]<<" ";
-        }
-        return ans;
+        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        int k=1;//k -> transactions allowed for this k=1
+        int ans=solve(k,0,true,prices,dp);
+        // return ans; // this is also correct
+        return dp[0][1];//dp[0][buy=true]
+
     }
 };
