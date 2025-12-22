95964fde5f403017273073b927320c7a3bbfc297
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2025-12-22 23:02:25 +0530

    Time: 3 ms (75.45%), Space: 27.6 MB (35.53%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -6,33 +6,39 @@
  *   TreeNode * right;
  *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ *right(right) {}
  *};
  */
-class Solution
-{
-    public:
-        TreeNode* build(vector<int> &pre, vector<int> &in, int &index, int strt, int end, map< int, int> &mpp)
-        {
-            if (index > in .size() || strt > end || strt > in .size()) return NULL;
-            int val = pre[index++];
-           	// val ka index in preorder 
-            int valIndex = mpp[val];
-            TreeNode *root = new TreeNode(val);
-            root->left = build(pre, in, index, strt, valIndex - 1, mpp);
-            root->right = build(pre, in, index, valIndex + 1, end, mpp);
-            return root;
-        }
+class Solution {
+public:
+    TreeNode* build(int &rootIndex, int start, int end, vector<int>& pre,
+        vector < int>& in, unordered_map<int, int>& mpp) {
+        if (rootIndex >= pre.size() || start > end || start>pre.size() || end<0)
+            return NULL;
 
-    TreeNode* buildTree(vector<int> &pre, vector<int> &in)
-    {
-        int n = in .size();
-        map<int, int> mpp;
-        for (int i = 0; i < n; i++)
-        {
-            mpp[ in[i]] = i;
-        }
-        int index = 0;
-        return build(pre, in, index, 0, n - 1,mpp);
+        int rootVal = pre[rootIndex++];
+        int ind = mpp[rootVal]; // current root val index in in_order
+
+        TreeNode* root = new TreeNode(rootVal);
+        root->left = build(rootIndex, start, ind - 1, pre, in,mpp); // subTree range start->ind-1
+        root->right =build(rootIndex, ind + 1, end, pre, in, mpp); // ind+1->end
+        return root;
     }
-    };
+
+    TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
+        // pre : root - left - right // assume rootIndex
+        // in : left - root - right
+        // we will find the root of every subtree from pre,
+        // respective left and right sub tree from in order(left: rootIndex-1,
+        // right:rootInd+1)
+        int n = pre.size();
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < n; i++)
+            mpp[in[i]] = i;
+        int rootIndex = 0; // first val in preorder is root of tree
+
+        return build(rootIndex, 0, n-1, pre, in, mpp);
+        // start,end  is the range of subtree
+    }
+};
