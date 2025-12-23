877a6c771266ebbb59941e9180cb62d1a51b0673
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2025-12-23 13:46:22 +0530

    Time: 16 ms (26.57%), Space: 22.5 MB (5.39%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,47 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
-    map<int,int>level;
-    map<TreeNode*,TreeNode*>parent;
-    void dfs(TreeNode* root,TreeNode* p=NULL,int l=0){
-        if(root==NULL)return;
-       parent[root]=p;
-        level[root->val]=l;
-        if(root->left)dfs(root->left,root,l+1);
-        if(root->right)dfs(root->right,root,l+1);
+    unordered_map<TreeNode*, TreeNode*> parent;
+    unordered_map<TreeNode*, int> level;
+
+    void dfs(TreeNode* root, TreeNode* p = nullptr, int l = 0) {
+        if (!root) return;
+        parent[root] = p;
+        level[root] = l;
+        dfs(root->left, root, l + 1);
+        dfs(root->right, root, l + 1);
     }
-    
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
         dfs(root);
-        int lev1=level[p->val];
-        int lev2=level[q->val];
-        
-            while(lev1>lev2){
-                p=parent[p];
-                lev1=level[p->val];
-            }
-        while(lev2>lev1){
-            q=parent[q];
-            lev2=level[q->val];
+
+        while (level[p] > level[q]) p = parent[p];
+        while (level[q] > level[p]) q = parent[q];
+        //both are same level
+        while (p != q) {
+            p = parent[p];
+            q = parent[q];
         }
-        
-            while(p!=q){
-                if(p==root||q==root){
-                    return root;
-                }
-                p=parent[p];
-                q=parent[q];
-            }
-            return p;
-        
+        return p;
     }
 };
