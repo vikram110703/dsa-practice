93ac0bfed57d89a8b27dcbcce59a759d146948f1
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2025-12-23 18:48:03 +0530

    Time: 103 ms (16.4%), Space: 131.3 MB (12.82%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void solve(int ind,int n,int k,vector<int>&curr){
-        if(ind>n || curr.size()>=k){
-            if(curr.size()==k)ans.push_back(curr);
+    vector<vector<int>> ans;
+
+    void solve(int ind, int n, int k, vector<int>& curr) {
+        if (curr.size() == k) {
+            ans.push_back(curr);
             return;
         }
-        //take
-        curr.push_back(ind);
-        solve(ind+1,n,k,curr);
+        if (ind > n) return;
+        // Pruning: not enough numbers left
+        if (curr.size() + (n - ind + 1) < k) return;
 
-        // backtrack
+        // take
+        curr.push_back(ind);
+        solve(ind + 1, n, k, curr);
         curr.pop_back();
-        solve(ind+1,n,k,curr);
+
+        // not take
+        solve(ind + 1, n, k, curr);
     }
+
     vector<vector<int>> combine(int n, int k) {
-        vector<int>curr;
-        solve(1,n,k,curr);
+        vector<int> curr;
+        solve(1, n, k, curr);
         return ans;
     }
 };
