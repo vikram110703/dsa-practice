e4b39e69097cee6b7da00b29587d12ef773cc4a3
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2025-12-24 15:35:25 +0530

    Time: 0 ms (100%), Space: 17.7 MB (16.53%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int n=nums.size();
-        int strt=0,end=n-1,mid=strt+(end-strt)/2;
-        if(n==0)return {-1,-1};
-        
-        while(end-strt>1)
+    int lower_bound(int start,int end,vector<int>&nums,int target){
+        int mid;
+        while(end-start>1)
         {
-            mid=strt+(end-strt)/2;
+            mid=start+(end-start)/2;
             if(nums[mid]>=target)
             {
                 end=mid;
             }
-            //<target
             else {
-                strt=mid+1;
+                start=mid+1;
             }
         }
-        // not found
-        if(nums[strt]!=target&&nums[end]!=target)
-        {
-            return {-1,-1};
-        }
-        vector<int>ans;
-        if(nums[strt]==target)ans.push_back(strt);
-        else ans.push_back(end);
-        
-        strt=0,end=n-1;
-        while(end-strt>1)
-        {
-            mid=strt+(end-strt)/2;
-            if(nums[mid]<=target)strt=mid;
-            //>target
-            else end=mid-1;
-        }
-        if(nums[end]==target)ans.push_back(end);
-        else ans.push_back(strt);
-        
-        return ans;
-        
+        //found ... return first occ
+        if(nums[start]==target)return start;
+        if(nums[end]==target)return end;
+
+        //not found ... return index of first greater element
+        if(nums[start]>target)return start;
+        if(nums[end]<target)return end+1;
+        return start+1;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        if(n==0)return{-1,-1};
+        int start=0,end=n-1;
+
+        int firstOcc=lower_bound(start,end,nums,target);
+        if(firstOcc>=n || nums[firstOcc]!=target)return{-1,-1};
+        int lastOcc=lower_bound(start,end,nums,target+1)-1;
+        if(nums[lastOcc]!=target)return {firstOcc,firstOcc};
+        return {firstOcc,lastOcc};
     }
 };
