8ad17aca02bc921daf406c83aa31a4795b986b2f
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2025-12-25 01:06:30 +0530

    Time: 25 ms (18.31%), Space: 18.4 MB (63.86%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -22,18 +22,23 @@ public:
         return lists.front();
     }
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if (l1 == nullptr) {
-            return l2;
+        if(l1==NULL)return l2;
+        if(l2==NULL)return l1;
+        ListNode* sortedList=new ListNode(0);
+        ListNode* tail=sortedList;
+        while(l1 && l2){
+            if(l1->val<=l2->val){
+                tail->next=l1;
+                l1=l1->next;
+            }
+            else{
+                tail->next=l2;
+                l2=l2->next;
+            }
+            tail=tail->next;
         }
-        if (l2 == nullptr) {
-            return l1;
-        }
-        if (l1->val <= l2->val) {
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        } else {
-            l2->next = mergeTwoLists(l1, l2->next);
-            return l2;
-        }
+        if(l1!=NULL)tail->next=l1;
+        if(l2!=NULL)tail->next=l2;
+        return sortedList->next;
     }
 };
