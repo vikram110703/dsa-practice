1cdab52f368ec6f8f4a8b4d932469ed3d92b2843
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2025-12-26 13:45:48 +0530

    Time: 0 ms (100%), Space: 13.2 MB (46.88%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,62 +1,26 @@
-class Solution
-{
-    public:
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int INF=1e9;
+    int n;
+    int limit=-1*1e5;
 
-        int cal(int prev, int row, vector<vector < int>> &tr, vector< vector< int>> &dp)
-        {
-            if (prev < 0||prev>=tr[row].size()) return INT_MAX;
-            int n = tr.size();
-            if (row == 0)
-            {
-                return dp[row][0]= tr[row][0];
-            }
-            if (dp[row][prev] != INT_MAX) return dp[row][prev];
+    int solve(int row,int col,vector<vector<int>>&triangle){
+        int maxColRange=row;//see in the triangle,col =row
+        if(row>=n || col>maxColRange || col<0) return INF;
+        if(row==n-1) return triangle[row][col];
+        if(dp[row][col]!=limit)return dp[row][col];
 
-            int mini = INT_MAX;
-       
-                int curr = tr[row][prev] + min(cal(prev, row - 1, tr, dp), cal(prev - 1, row - 1, tr, dp));
-                mini = min(mini, curr);
-            // cout<<row<<" "<<mini<<endl;
-            
-            return dp[row][prev] = mini;
-        }
+        int op1=solve(row+1,col,triangle);
+        int op2=solve(row+1,col+1,triangle);
+        return dp[row][col]=triangle[row][col]+min(op1,op2);
+    }
 
-    int minimumTotal(vector<vector < int>> &tr)
-    {
-
-        // int n = tr.size();
-        // if(n==1)return tr[0][0];
-        // vector<vector < int>> dp(n, vector<int> (n+1 , INT_MAX));
-        // int mini=INT_MAX;
-        // for(int i=0;i<n;i++)
-        // {
-        //   int curr=cal(i, n-1, tr, dp);
-        //     mini=min(mini,curr);
-        // }
-        // return mini;
-        
-        //tabulation 
-        
-        int n=tr.size();
-        vector<vector<int>>dp(n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                if(i==0){
-                    dp[i].push_back(tr[0][0]);
-                }
-                else{
-                    int mini=INT_MAX;
-                    if(j!=0){
-                        mini=min(mini,tr[i][j]+dp[i-1][j-1]);
-                    }
-                    if(j!=i){
-                        mini=min(mini,tr[i][j]+dp[i-1][j]);
-                    }
-                    dp[i].push_back(mini);
-                }
-            }
-        }
-        return *min_element(dp[n-1].begin(),dp[n-1].end());
-       
+    int minimumTotal(vector<vector<int>>& triangle) {
+        n=triangle.size();
+        dp.resize(n+1,vector<int>(n+1,limit));
+        //we can go only (i+1,j) or (i+1,j+1)
+        int ans=solve(0,0,triangle);
+        return ans;
     }
 };
