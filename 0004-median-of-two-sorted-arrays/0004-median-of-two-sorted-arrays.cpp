7c3d30045761ce5ad0238acd62e302cacf6b804e
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2025-12-27 23:46:59 +0530

    Time: 3 ms (29.71%), Space: 95 MB (93.14%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,72 +1,32 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int i=0,j=0;
-        int n=nums1.size()+nums2.size();
-        int cnt=0;double ans=0.0,ans2=0.0;
-        int ind=n/2;
-        if(n%2==0)ind-=1;
-    
-            while(i<nums1.size()||j<nums2.size()){
-                if(i<nums1.size()&&j<nums2.size()){
-                    if(nums2[j]>nums1[i]){
-                        
-                        if(cnt==ind){
-                            // cout<<"yes "<<i<<" "<<j<<endl;
-                            ans+=(nums1[i]*1.0);
-                        }
-                        if(cnt==ind+1){
-                            ans2+=(nums1[i]*1.0);
-                            break;
-                        }
-                        i+=1;
-                    }
-                    else{
-                        
-                        
-                         if(cnt==ind){
-                             // cout<<" 2 : "<<i<<" "<<j<<endl;
-                            ans+=(nums2[j]*1.0);
-                        }
-                        if(cnt==ind+1){
-                            ans2+=(nums2[j]*1.0);
-                            break;
-                        }
-                        j+=1;
-                    }
-                    // cnt+=1;
+        int n = nums1.size();
+        int m = nums2.size();
+        int i = 0, j = 0, m1 = 0, m2 = 0;
+
+        // Find median.
+        for (int count = 0; count <= (n + m) / 2; count++) {
+            m2 = m1;
+            if (i != n && j != m) {
+                if (nums1[i] > nums2[j]) {
+                    m1 = nums2[j++];
+                } else {
+                    m1 = nums1[i++];
                 }
-                else if(i<nums1.size()){
-                     if(cnt==ind){
-                            ans+=(nums1[i]*1.0);
-                        }
-                        if(cnt==ind+1){
-                            ans2+=(nums1[i]*1.0);
-                            break;
-                        }
-                    i+=1;
-                }
-                else{
-                     if(cnt==ind){
-                            ans+=(nums2[j]*1.0);
-                        }
-                        if(cnt==ind+1){
-                            ans2+=(nums2[j]*1.0);
-                            break;
-                        }
-                    j+=1;
-                }
-                cnt+=1;
+            } else if (i < n) {
+                m1 = nums1[i++];
+            } else {
+                m1 = nums2[j++];
             }
-        cout<<cnt<<endl;
-    if(n%2==0)
-    {
-        return (ans+ans2)/(2.0);
-    }
-    else {
-        return (ans*1.0);
-    }
-        
-      
+        }
+
+        // Check if the sum of n and m is odd.
+        if ((n + m) % 2 == 1) {
+            return static_cast<double>(m1);
+        } else {
+            double ans = static_cast<double>(m1) + static_cast<double>(m2);
+            return ans / 2.0;
+        }
     }
 };
