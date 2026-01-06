4b2c7b7e398a3dbb063016270cd8aed4bad44f96
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2026-01-06 22:15:19 +0530

    Time: 3 ms (65.05%), Space: 24 MB (5.71%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -7,38 +7,50 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
+ 
+    TIME :   O(Nlog(K))
+    SPACE :  O(log(K))
  */
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) {
-            return nullptr;
-        }
-        while (lists.size() > 1) {
-            lists.push_back(mergeTwoLists(lists[0], lists[1]));
-            lists.erase(lists.begin());
-            lists.erase(lists.begin());
-        }
-        return lists.front();
+        if (lists.empty()) return NULL;
+        return mergeRange(lists, 0, lists.size() - 1);
     }
+
+    ListNode* mergeRange(vector<ListNode*>& lists, int left, int right) {
+        if (left == right) return lists[left];
+
+        int mid = left + (right - left) / 2;
+
+        ListNode* l1 = mergeRange(lists, left, mid);
+        ListNode* l2 = mergeRange(lists, mid + 1, right);
+
+        return mergeTwoLists(l1, l2);
+    }
+
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1==NULL)return l2;
-        if(l2==NULL)return l1;
-        ListNode* sortedList=new ListNode(0);
-        ListNode* tail=sortedList;
-        while(l1 && l2){
-            if(l1->val<=l2->val){
-                tail->next=l1;
-                l1=l1->next;
+        if (l1 == NULL) return l2;
+        if (l2 == NULL) return l1;
+
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
             }
-            else{
-                tail->next=l2;
-                l2=l2->next;
-            }
-            tail=tail->next;
+            tail = tail->next;
         }
-        if(l1!=NULL)tail->next=l1;
-        if(l2!=NULL)tail->next=l2;
-        return sortedList->next;
+
+        if (l1 != NULL) tail->next = l1;
+        if (l2 != NULL) tail->next = l2;
+
+        return dummy->next;
     }
 };
