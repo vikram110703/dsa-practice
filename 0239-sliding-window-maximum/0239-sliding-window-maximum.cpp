59ccee512ea3104130ac5ea8d13ebfa5215938e3
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2026-01-07 17:22:01 +0530

    Time: 75 ms (12.26%), Space: 153.3 MB (11.84%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,45 +1,52 @@
 class Solution {
 public:
-    
-    
-void build(int ind, int low, int high, vector<int> &a, vector<int> &seg)
-{
-    if (low == high)
-    {
-        seg[ind] = a[low];
-        return;
-    }
-    int mid = low + (high - low) / 2;
-    build(2 * ind + 1, low, mid, a, seg);
-    build(2 * ind + 2, mid + 1, high, a, seg);
-    seg[ind] = max(seg[(2 * ind) + 1],seg[(2 * ind) + 2]);
-}
+//time o(nlog(n))
+//space o(n)
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>> pq; // (value, index)
+        vector<int> ans;
 
-int query(int ind, int low, int high, int l, int r, vector<int> &seg)
-{
-    if (high < l || low > r)
-        return INT_MIN;
-    if (low >= l && high <= r)
-        return seg[ind];
-    int mid = low + (high - low) / 2;
-    int left = query((2 * ind) + 1, low, mid, l, r, seg);
-    int right = query((2 * ind) + 2, mid + 1, high, l, r, seg);
-    return max(left,right);
-}
-    
-    vector<int> maxSlidingWindow(vector<int>& a, int k) {
-        int n=a.size();
-        vector<int>seg(4*n,0),ans;
-        build(0,0,n-1,a,seg);
-        int strt=0;
-        for(int i=k-1;i<n;i++)
-        {
-            int maxi=query(0,0,n-1,strt,i,seg);
-            // cout<<strt<<" "<<k<<endl;
-            ans.push_back(maxi);
-            strt++;
+        for (int i = 0; i < nums.size(); i++) {
+            pq.push({nums[i], i});
+
+            // window starts forming when i >= k - 1
+            if (i >= k - 1) {
+                // remove elements outside window
+                while (!pq.empty() && pq.top().second <= i - k) {
+                    pq.pop();
+                }
+                ans.push_back(pq.top().first);
+            }
         }
         return ans;
-       
     }
 };
+
+// deque SOLUTION 
+// time O(n) space o(k) , maintain a decreasing order deque ... [fronnt -----back]
+// vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         deque<int> dq;  // stores indices
+//         vector<int> ans;
+
+//         for (int i = 0; i < nums.size(); i++) {
+
+//             // 1. Remove indices out of window
+//             if (!dq.empty() && dq.front() <= i - k) {
+//                 dq.pop_front();
+//             }
+
+//             // 2. Maintain decreasing order in deque
+//             while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+//                 dq.pop_back();
+//             }
+
+//             // 3. Add current index
+//             dq.push_back(i);
+
+//             // 4. Record answer once window is valid
+//             if (i >= k - 1) {
+//                 ans.push_back(nums[dq.front()]);
+//             }
+//         }
+//         return ans;
+//     }
