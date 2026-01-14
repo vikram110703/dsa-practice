413cfd7f6bf764eb2b0a401846bf494f55819444
Vikram Singh <99256244+vikram110703@users.noreply.github.com>
2026-01-14 14:07:54 +0530

    Time: 0 ms (100%), Space: 77.2 MB (43.63%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,53 +1,30 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* head = l1;
-        ListNode* prev = nullptr;
+
+        ListNode dummy(0);           // ✅ stack allocation
+        ListNode* tail = &dummy;
         int carry = 0;
 
-        while (l1 != nullptr && l2 != nullptr) {
-            int sum = l1->val + l2->val + carry;
+        while (l1 || l2 || carry) {
+            int sum = carry;
+
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
             carry = sum / 10;
-            l1->val = sum % 10;
+            sum %= 10;
 
-            prev = l1;
-            l1 = l1->next;
-            l2 = l2->next;
+            tail->next = new ListNode(sum);// this should be always heap allocated
+            tail = tail->next;
         }
 
-        while (l1 != nullptr) {
-            int sum = l1->val + carry;
-            carry = sum / 10;
-            l1->val = sum % 10;
-
-            prev = l1;
-            l1 = l1->next;
-        }
-
-        while (l2 != nullptr) {
-            int sum = l2->val + carry;
-            carry = sum / 10;
-
-            prev->next = new ListNode(sum % 10);
-            prev = prev->next;
-            l2 = l2->next;
-        }
-
-        if (carry) {
-            prev->next = new ListNode(carry);
-        }
-
-        return head;
+        return dummy.next;
     }
 };
